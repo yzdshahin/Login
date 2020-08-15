4ea25ed67ff9d10220ff9e22d295d3f3618c70b1
@yzdshahin <shahin234yazdani@gmail.com>
2020-08-15 23:06:56 +0430

    commiting the view

--- src/View/LogInView.java
+++ src/View/LogInView.java
@@ -1,10 +1,15 @@
 package View;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  * Created by shahin_yzd on 8/14/2020.
  */
-public class LogInView {
-    public static void main(String[] args) {
+public class LogInView extends HttpServlet {
 
-    }
+
 }
