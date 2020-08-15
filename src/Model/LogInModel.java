37a69fd71cfcd098efd67585105e76d8ac169047
@yzdshahin <shahin234yazdani@gmail.com>
2020-08-15 11:31:00 +0430

    commiting the model, view and controller of login into git

--- src/Model/LogInModel.java
+++ src/Model/LogInModel.java
@@ -1,7 +1,29 @@
 package Model;
-
 /**
  * Created by shahin_yzd on 8/15/2020.
  */
 public class LogInModel {
+
+    public class Person{
+        private String user;
+        private String pass;
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public String getPass() {
+            return pass;
+        }
+
+        public void setPass(String pass) {
+            this.pass = pass;
+        }
+
+
+    }
 }
