98673830483b7e17f44decd47e6f9a7d4dbd0783
@yzdshahin <shahin234yazdani@gmail.com>
2020-08-16 22:23:25 +0430

    control and obj

--- src/Controller/LogInControl.java
+++ src/Controller/LogInControl.java
@@ -1,7 +1,31 @@
 package Controller;
 
+import Model.Person;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by shahin_yzd on 8/15/2020.
  */
-public class LogInControl {
+public class LogInControl extends HttpServlet {
+
+
+    public void Pot(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html"); // by this we show our output type
+        PrintWriter writer = response.getWriter();
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        Person person = new Person();
+        person.setUser(name);
+        person.setPass(password);
+
+        request.setAttribute("Person", person);
+
+    }
 }
