935ad10eacd4c507d3026caa89dcec2b053bc1b7
@yzdshahin <shahin234yazdani@gmail.com>
2020-08-16 22:22:57 +0430

    view edit

--- src/View/LogInView.java
+++ src/View/LogInView.java
@@ -13,25 +13,12 @@ import javax.servlet.http.HttpServletResponse;
  * Created by shahin_yzd on 8/14/2020.
  * https://www.javatpoint.com/MVC-in-jsp
  */
-public class LogInView extends HttpServlet {
-    public void Pot(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html"); // ny this we show our output type
-        PrintWriter writer = response.getWriter();
-
-        String name = request.getParameter("name");
-        String password = request.getParameter("password");
-
-        Person person=new Person();
-        person.setUser(name);
-        person.setPass(password);
-
-        request.setAttribute("Person",person);
+public class LogInView {
 
 
 
 
 
-    }
 
 
 
