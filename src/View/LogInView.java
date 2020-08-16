9b7cc77a2c9821d633d26f9286e649710e36008a
@yzdshahin <shahin234yazdani@gmail.com>
2020-08-16 21:30:21 +0430

    creating obj from model class

--- src/View/LogInView.java
+++ src/View/LogInView.java
@@ -1,4 +1,7 @@
 package View;
+import Model.LogInModel;
+import Model.Person;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
@@ -8,8 +11,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /**
  * Created by shahin_yzd on 8/14/2020.
+ * https://www.javatpoint.com/MVC-in-jsp
  */
 public class LogInView extends HttpServlet {
+    public void Pot(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html"); // ny this we show our output type
+        PrintWriter writer = response.getWriter();
 
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        Person person=new Person();
+        person.setUser(name);
+        person.setPass(password);
+
+        request.setAttribute("Person",person);
+
+
+
+
+
+    }
+
+
+    }
 
 }
