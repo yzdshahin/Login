c525bb7a4648daa6249b63b94956b1c4468ae565
@yzdshahin <shahin234yazdani@gmail.com>
2020-08-21 21:24:37 +0430

    kolesh

---
+++ src/Model/Person.java
@@ -0,0 +1,25 @@
+package Model;
+
+/**
+ * Created by shahin_yzd on 8/21/2020.
+ */
+public class Person {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
